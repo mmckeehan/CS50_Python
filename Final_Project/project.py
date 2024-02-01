13034c9a10cd771d0159cdb1b6d3a435c6554ff0
Michael McKeehan <mmckeeha@gmail.com>
2024-01-31 21:06:30 -0500

    Added validation for command line arugments and unit test. Starting on the CSV class.

--- Final_Project/project.py
+++ Final_Project/project.py
@@ -10,8 +10,39 @@
 import sys
 import csv
 
-def main():
+# This class will handle the CSV.
+class Input_CSV():
     ...
+
+
+def main():
+    data_file = correct_args(sys.argv)
+
+    if data_file == None:
+        sys.exit("The selected file is not a CSV")
+
+    date_range(data_file)
+
+# Get the desired date range from the user
+def date_range(df):
+    dates = Input_CSV.date_range(df)
+
+# Checks to see if there are only two command line arugments
+def correct_args(df: str):
+    if len(df) < 2:
+        sys.exit(ValueError("Too Few Arugments"))
+    elif len(df) < 2:
+        sys.exit(ValueError("Too Few Arugments"))
+    else:
+        confirm_csv(df[1])
+        return df[1]
+
+# Verifies that the second command line arugment is a CSV
+def confirm_csv(df: str):
+    if df.endswith(".csv"):
+        return df
+    else:
+        return None
 
 if __name__ == "__main__":
     main()
--- Final_Project/test_project.py
+++ Final_Project/test_project.py
@@ -1,7 +1,8 @@
 import project
 
-def test_function_1():
-    ...
+def test_confirm_csv():
+    assert project.confirm_csv("TestCSV.csv") == "TestCSV.csv"
+    assert project.confirm_csv("README.MD") == None
 
 
 def test_function_2():
