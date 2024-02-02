bddee6c53d9409a9e1fcdea1d193d128df29376b
Michael McKeehan <mmckeeha@gmail.com>
2024-02-01 21:41:17 -0500

    Change project to game... I feel better about this. Created bones for Room class.

---
+++ Final_Project/Room.py
@@ -0,0 +1,22 @@
+class Room()
+    def __init__(self):
+        ...
+
+    def __str__(self):
+        ...
+
+    @property
+    def name(self):
+        ...
+
+    @property
+    def description(self):
+        ...
+
+    @property
+    def item(self):
+        ...
+
+    @property
+    def trap(self):
+        ...
--- Final_Project/test_project.py
+++ Final_Project/test_project.py
@@ -1,8 +1,8 @@
 import project
+import pytest
 
-def test_confirm_csv():
-    assert project.confirm_csv("TestCSV.csv") == "TestCSV.csv"
-    assert project.confirm_csv("README.MD") == None
+def test_function_1():
+    ...
 
 
 def test_function_2():
